41ad09781e20afb78cee000bb513dfb3eb045813
Amanda Amaral <54366454+dinhoka@users.noreply.github.com>
2021-05-10 21:23:54 -0300

    Add files via upload

---
+++ exercicio1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<h1>VERIFICAÇÃO DE VALOR</h1>
+<?php
+
+$val = 0;
+
+
+if ($val >0){
+echo "O valor é positivo";
+
+
+}else if ($val < 0) {
+	echo "O valor é negativo";
+}else{
+	echo "Ovalor é igual a 0";
+}
+
+
+?>
+</body>
+</html>
